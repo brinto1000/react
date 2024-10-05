3eebc59a049f982d27591eda644f55a5dbe00a35
brinto1000 <62906634+brinto1000@users.noreply.github.com>
2024-10-05 10:28:46 +0600

    Create app.jsx

---
+++ app.jsx
@@ -0,0 +1,13 @@
+const Home = React.lazy(() => import('./Home'));
+const About = React.lazy(() => import('./About'));
+
+function App() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Suspense>
+  );
+}
